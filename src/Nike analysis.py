74478ae6967c91463d17a693b65d3dac95778eef
Julian Rasch <rasch@westphalia-datalab.com>
2019-08-14 15:35:41 +0200

    a small fix and explanation for string splitting problems

--- src/Nike analysis.py
+++ src/Nike analysis.py
@@ -21,10 +21,17 @@ print(new_df)
 # dropping null value columns to avoid errors
 new_df.dropna(inplace = True)
 
+# JULIAN: You have almost had it there! The problem is, that you need something
+# called regular expressions ("regex") to define the split. The symbol you want
+# to split on is " or ', which are special symbols in Python (and R), since they
+# define the beginning of a string (as in path = "your_file.csv" for example). So
+# in order to use them as a character, you need to put an "escape"-character in
+# front of them, which is a backslash \. The expression below now works and does
+# a second thing: with | you can say "or", so this splits on either
+# b' or b".
+
 # new data frame with split value columns
-new = new_df["tweet"].str.split("b'", n = 1, expand = True)
-# making separate b' columns
-new_df["b'"] = new[0]
+new = new_df["tweet"].str.split("b\'|b\"", n = 1, expand = True)
 # making separate tweet column
 new_df["tweet"] = new[1]
 
