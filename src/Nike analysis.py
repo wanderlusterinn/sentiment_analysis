5e5d1540577a5e4d38846752e47b0fe99adc68dc
Julian Rasch <rasch@westphalia-datalab.com>
2019-08-21 21:23:35 +0200

    - some more explanations on string splitting

--- src/Nike analysis.py
+++ src/Nike analysis.py
@@ -38,8 +38,27 @@ new_df.dropna(inplace = True)
 new = new_df["tweet"].str.split("b\'|b\"", n=1, expand=True)
 new_df["tweet"] = new[1]
 
-
+# JULIAN: The problem is, you're doing the operation on all rows, but some
+# of your rows do not begin with RT and the stuff behind that. A second problem
+# is, that some of your rows also have an underscore in there (example: 2nd row),
+# so [a-zA-z] will not work. .* can be used for "everything".
 new = new_df["tweet"].str.split("RT\s@+[a-zA-z]+:", n=1, expand=True)
+
+# JULIAN: this does the split better. However, this is going to always give you
+# 2 columns (since you specified that), one for the content before and one for
+# the content behind the splitting pattern.
+# So whenever the pattern exists you're gonna get an empty first column
+# and the second column without that pattern, whenever it does not exist, you're
+# gonna get everything unchanged in the first column, and "None" in the second:
+# there was no split to be made.
+new = new_df["tweet"].str.split("RT\s@.*:", n=1, expand=True)
+
+# JULIAN: but I guess you rather want to simply delete the part with "RT...", if
+# it exists? Then you could got for str.replace and simply replace the pattern by
+# nothing: ""
+new = new_df["tweet"].str.replace("RT\s@.*:", "")
+
+# JULIAN: now maybe continue here on your own! :)
 new_df["tweet"] = new[1]
 new_df["rest"] = new[0]
 print(new_df)
