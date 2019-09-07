062abdc60fd65162c7a370e0c8bd402e737b6985
Thecuba <theresa.kuban@gmail.com>
2019-09-07 13:46:44 +0200

    Neue Schritte der Datenbereiningung und mehr

--- src/Nike analysis.py
+++ src/Nike analysis.py
@@ -1,70 +1,113 @@
 import pandas as pd
+import string
 import re
-import csv
-import preprocessor as p
+import nltk
+#nltk.download()
+from sklearn.feature_extraction.text import CountVectorizer
 
-# determine the path for the data
+
+# file = '../data/tweetsAvengersEndgame.csv'
 file = "../data/result_nike_betsyross.csv"
 
-# create a dataframe
-# seperate the csv in columns after ;
-df = pd.read_csv(file, sep=';', header = None)
+# read in the data
+df = pd.read_csv(file, sep=";",encoding = "ISO-8859-1")
 
-# creating a header/naming the columns
+# rename columns
 df.columns = ['date','time','tweet']
 
-# deleting all columns besides 'tweet'
-# create a new csv file
-keep_col = ['tweet']
-new_df = df[keep_col]
-new_df.to_csv("tweetsnike.csv", index=False)
+#Nur Wortinhalte behalten
+df["tweet"] = df["tweet"].str.replace("b\'|b\"", "")
+df["tweet"] = df["tweet"].str.replace("RT\s@.*:", "")
 
-print(new_df)
+#eigene Funktion erstellen; Punctuation entfernen
+def remove_punct(text):
+    text  = "".join([char for char in text if char not in string.punctuation])
+    text = re.sub('[0-9]+', '', text)
+    return text
 
-# dropping null value columns to avoid errors
-new_df.dropna(inplace = True)
+df['Tweet_punct'] = df['tweet'].apply(lambda x: remove_punct(x))
+df.head(10)
 
-# JULIAN: You have almost had it there! The problem is, that you need something
-# called regular expressions ("regex") to define the split. The symbol you want
-# to split on is " or ', which are special symbols in Python (and R), since they
-# define the beginning of a string (as in path = "your_file.csv" for example). So
-# in order to use them as a character, you need to put an "escape"-character in
-# front of them, which is a backslash \. The expression below now works and does
-# a second thing: with | you can say "or", so this splits on either
-# b' or b".
+#Tokenization
+def tokenization(text):
+    text = re.split('\W+', text)
+    return text
 
-# new data frame with split value columns
+df['Tweet_tokenized'] = df['Tweet_punct'].apply(lambda x: tokenization(x.lower()))
+df.head()
 
-new = new_df["tweet"].str.split("b\'|b\"", n=1, expand=True)
-new_df["tweet"] = new[1]
-
-# JULIAN: The problem is, you're doing the operation on all rows, but some
-# of your rows do not begin with RT and the stuff behind that. A second problem
-# is, that some of your rows also have an underscore in there (example: 2nd row),
-# so [a-zA-z] will not work. .* can be used for "everything".
-new = new_df["tweet"].str.split("RT\s@+[a-zA-z]+:", n=1, expand=True)
-
-# JULIAN: this does the split better. However, this is going to always give you
-# 2 columns (since you specified that), one for the content before and one for
-# the content behind the splitting pattern.
-# So whenever the pattern exists you're gonna get an empty first column
-# and the second column without that pattern, whenever it does not exist, you're
-# gonna get everything unchanged in the first column, and "None" in the second:
-# there was no split to be made.
-new = new_df["tweet"].str.split("RT\s@.*:", n=1, expand=True)
-
-# JULIAN: but I guess you rather want to simply delete the part with "RT...", if
-# it exists? Then you could got for str.replace and simply replace the pattern by
-# nothing: ""
-new = new_df["tweet"].str.replace("RT\s@.*:", "")
-
-# JULIAN: now maybe continue here on your own! :)
-new_df["tweet"] = new[1]
-new_df["rest"] = new[0]
-print(new_df)
+#Remove stopwords
+stopword = nltk.corpus.stopwords.words('english')
 
 
-tweet_col = new_df["rest"] + new_df["tweet"]
-print(tweet_col)
+def remove_stopwords(text):
+    text = [word for word in text if word not in stopword]
+    return text
+
+
+df['Tweet_nonstop'] = df['Tweet_tokenized'].apply(lambda x: remove_stopwords(x))
+df.head(10)
+
+#Stammwörter bilden
+ps = nltk.PorterStemmer()
+
+def stemming(text):
+    text = [ps.stem(word) for word in text]
+    return text
+
+df['Tweet_stemmed'] = df['Tweet_nonstop'].apply(lambda x: stemming(x))
+df.head()
+
+#Frage: wie gehen wir mit Nichtwörtern um? Was machen wir damit?
+
+wn = nltk.WordNetLemmatizer()
+
+def lemmatizer(text):
+    text = [wn.lemmatize(word) for word in text]
+    return text
+
+df['Tweet_lemmatized'] = df['Tweet_nonstop'].apply(lambda x: lemmatizer(x))
+df.head()
+
+def delete_stuff(text):
+    text = [word for word in text if word.find("http") == -1 and word.find("xx") == -1]
+    return text
+
+df['Tweet_ohne_stuff'] = df['Tweet_lemmatized'].apply(lambda x: delete_stuff(x))
+df.head()
+#Umgang mit Wörtern - exxa, etc.
+
+#eigene Funktion definieren, die alle Schritte vroehr beinhaltet
+def clean_text(text):
+    text_lc = "".join([word.lower() for word in text if word not in string.punctuation]) # remove puntuation
+    text_rc = re.sub('[0-9]+', '', text_lc)
+    tokens = re.split('\W+', text_rc)    # tokenization
+    text = [ps.stem(word) for word in tokens if word not in stopword]  # remove stopwords and stemming
+    text = [word for word in text if word.find("http") == -1 and word.find("xx") == -1]
+    return text
+
+#Vectorization
+countVectorizer = CountVectorizer(analyzer=clean_text)
+countVector = countVectorizer.fit_transform(df['tweet'])
+print('{} Number of tweets has {} words'.format(countVector.shape[0], countVector.shape[1]))
+# 11412 Number of tweets has 9248 words
+
+n = countVector.shape[0]
+m = countVector.shape[1]
+#count_vect_df = pd.DataFrame(countVector.toarray(), columns=countVectorizer.get_feature_names())
+count_vect_df = pd.DataFrame(countVector[0:n, 0:m].toarray(), columns=countVectorizer.get_feature_names()[0:m])
+count_vect_df.head()
+
+# Aussuchen der meistgenannten Wörter - probeweise erstmal die wichtigsten 100 genommen
+word_count = count_vect_df.sum(axis=0)
+word_count_sorted = word_count.sort_values(ascending=False)
+important_words = word_count_sorted[0:100]
+important_words.index
+
+# data frame mit den wichtigsten Worten (s.oben)
+df_important_words = count_vect_df[important_words.index]
+
+#Wordcloud Visualization
+from wordcloud import WordCloud, STOPWORDS , ImageColorGenerator
 
 
